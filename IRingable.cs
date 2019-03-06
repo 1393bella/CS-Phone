bd7ac9f5be6164ba87276846f56e3fde3bfc5ba9
Narciso Lobo <narcisolobo@gmail.com>
2019-03-06 15:08:55 -0600

    first commit

---
+++ IRingable.cs
@@ -0,0 +1,9 @@
+namespace Phone {
+    public interface IRingable {
+        // Create an IRingable interface that contains the following methods:
+        // Ring () : This method returns a string
+        // Unlock () : This method returns a string
+        string Ring();
+        string Unlock();
+    }
+}
